b35aecc10015a75f4f663dc3c49d2546f6a5f11b
MahammedJafarzade <7a4m9bb@code.edu.az>
2024-01-30 10:32:49 +0400

    push notification

--- app/src/main/java/com/mahammadjafarzade/pushnotification/FirebasePushNotificationService.kt
+++ app/src/main/java/com/mahammadjafarzade/pushnotification/FirebasePushNotificationService.kt
@@ -1,11 +1,46 @@
 package com.mahammadjafarzade.pushnotification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.util.Date
 
 class FirebasePushNotificationService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+
+        val resultIntent = Intent(this,MainActivity::class.java)
+        resultIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+
+
+        val taskStackBuilder : TaskStackBuilder = TaskStackBuilder.create(this)
+        taskStackBuilder.addNextIntent(resultIntent)
+
+        var requestCode = 1001
+        val pendingIntent : PendingIntent = taskStackBuilder.getPendingIntent(requestCode,PendingIntent.FLAG_IMMUTABLE)
+
+        val channelId = "tanitim"
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("App title")
+            .setContentText("App text")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setAutoCancel(false)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(channelId,"Tanitim Bildirimleri",NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(channel)
+        }
+        val id = Date().time.toInt()
+        notificationManager.notify(id,notificationBuilder.build())
 
 
 
